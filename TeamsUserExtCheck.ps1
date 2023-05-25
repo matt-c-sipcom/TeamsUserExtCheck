28b5308a2242a2896baf3c294e95f4c956f8bf30
matt-c-sipcom <125553587+matt-c-sipcom@users.noreply.github.com>
2023-05-25 10:09:55 +0100

    Add files via upload

---
+++ TeamsUserExtCheck.ps1
@@ -0,0 +1,26 @@
+﻿# Connect to Microsoft Teams
+Connect-MicrosoftTeams
+
+# Get the current date and format it
+$date = Get-Date -Format yyyy-MM-dd
+
+# Get the tenant display name, or set it to 'Unknown' if it cannot be retrieved
+try {
+    $tenant = (Get-CsTenant).DisplayName
+}
+catch {
+    Write-Host "Error retrieving tenant display name: $_"
+    $tenant = 'Unknown'
+}
+
+# Get the results of the search
+$results1 = Get-CsOnlineUser | Where-Object LineURI -match 'x' | Select-Object UserPrincipalName, LineUri
+
+# If there are results, print them; otherwise, display a message
+if ($results1) {
+    Write-Host "Microsoft Teams-enabled users with extensions in their direct routing number found and listed in the table below.`n`n"
+    $results1 | Format-Table UserPrincipalName, LineUri
+}
+else {
+    Write-Host "No Microsoft Teams-enabled users with extensions in their direct routing number found.`n`n"
+}
